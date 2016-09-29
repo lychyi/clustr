3a252d7a8f14a3246fe4614a34610528a76ca622
Chyi, Lynn <lchyi@illumina.com>
2016-09-29 14:34:38 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PlotComponentComponent } from './plot-component/plot-component.component';
+
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlotComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
